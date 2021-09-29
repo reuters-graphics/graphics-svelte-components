ec3021461cab0f1448933b1eefabd7557d87bd43
MinamiFunakoshiTR <minami.funakoshi@thomsonreuters.com>
2021-09-29 12:45:23 -0500

    fixed ai2svelte component, added example to demo

--- src/lib/Ai2svelte/index.svelte
+++ src/lib/Ai2svelte/index.svelte
@@ -1,47 +1,29 @@
 <script>
   import { onMount } from 'svelte';
-  import marked from 'marked';
 
-  export let title;
+  export let path;
   export let id;
-  export let chatter;
-  export let source;
-  export let note;
   export let size;
 
   let Ai2svelteGraphic = null;
-  let filePath = `./ai2html/${id}.svelte`;
 
   onMount(async () => {
     try {
-      Ai2svelteGraphic = (await import(filePath)).default;
+      Ai2svelteGraphic = (await import(path)).default;
     } catch (e) {
-      console.log(
-        'Error with your ai2svelte. Check Ai2svelte.svelte component',
-        e
-      );
+      console.log(`Unable to load ai2svelte component at: ${path}.`, e);
     }
   });
 </script>
 
 <section class="ai2svelte graphic {size}" id="{id}">
-  {#if title}
-    <h3>{title}</h3>
-  {/if}
-  {#if chatter}
-    <p>{chatter}</p>
+  {#if $$slots.title}
+    <slot name="title" />
   {/if}
 
   <svelte:component this="{Ai2svelteGraphic}" />
 
-  <aside>
-    {#if note}
-      <p class="note">
-        Note: {@html marked.parseInline(note)}
-      </p>
-    {/if}
-    {#if source}
-      <p class="source">Source: {@html marked.parseInline(source)}</p>
-    {/if}
-  </aside>
+  {#if $$slots.notes}
+    <slot name="notes" />
+  {/if}
 </section>
