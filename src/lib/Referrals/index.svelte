cce8e574627a2ea541351158907e74579b87179a
hobbes7878 <jon.r.mcclure@gmail.com>
2022-03-09 13:10:53 +0000

    logo options for referrals. closes #36

--- src/lib/Referrals/index.svelte
+++ src/lib/Referrals/index.svelte
@@ -4,14 +4,38 @@
   import IntersectionObserver from './IntersectionObserver.svelte';
 
   export let referrals = [];
+
+  export let monocolourLogo = false;
+  export let lightLogo = false;
 </script>
 
 <section class="referral-container">
   <figure>
-    <img
-      src="https://graphics.thomsonreuters.com/style-assets/images/logos/reuters-logo/svg/logo-color.svg"
-      alt="Reuters logo"
-    />
+    {#if monocolourLogo && lightLogo}
+      <img
+        class="logo reuters-graphics"
+        src="https://graphics.thomsonreuters.com/style-assets/images/logos/reuters-logo/svg/reuters-logo-light.svg"
+        alt="Reuters logo"
+      />
+    {:else if monocolourLogo}
+      <img
+        class="logo reuters-graphics"
+        src="https://graphics.thomsonreuters.com/style-assets/images/logos/reuters-logo/svg/reuters-logo-dark.svg"
+        alt="Reuters logo"
+      />
+    {:else if lightLogo}
+      <img
+        class="logo reuters-graphics"
+        src="https://graphics.thomsonreuters.com/style-assets/images/logos/reuters-logo/svg/reuters-logo-color-light.svg"
+        alt="Reuters logo"
+      />
+    {:else}
+      <img
+        class="logo reuters-graphics"
+        src="https://graphics.thomsonreuters.com/style-assets/images/logos/reuters-logo/svg/reuters-logo-color-dark.svg"
+        alt="Reuters logo"
+      />
+    {/if}
   </figure>
 
   {#if browser}
