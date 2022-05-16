b7cf877fe8181c9674605a5a9d907b51bb76b4f7
deaxmachina <d.y.bankova@gmail.com>
2022-05-16 14:25:31 +0100

    added well side options and slots for title chatter and notes

--- src/lib/DatawrapperChart/index.svelte
+++ src/lib/DatawrapperChart/index.svelte
@@ -3,43 +3,56 @@
   export let ariaLabel = 'Arial label';
   export let id = 'id';
   export let src = '';
-  export let maxWidth = 660;
   export let scrolling = 'no';
-
-  // title="Global abortion access"
-  // ariaLabel="Stacked Bars"
+  export let size = 'normal'; // options: wide, wider, widest, fluid
 </script>
 
-<div class="datawrapper-chart" style="{`max-width: ${maxWidth}px`}">
-  <iframe
-    title="{title}"
-    aria-label="{ariaLabel}"
-    id="{id}"
-    src="{src}"
-    scrolling="{scrolling}"
-    frameborder="0"
-    style="width: 0; min-width: 100% !important; border: none;"></iframe>
-  <script type="text/javascript">
-    !(function () {
-      'use strict';
-      window.addEventListener('message', function (e) {
-        if (void 0 !== e.data['datawrapper-height']) {
-          var t = document.querySelectorAll('iframe');
-          for (var a in e.data['datawrapper-height'])
-            for (var r = 0; r < t.length; r++) {
-              if (t[r].contentWindow === e.source)
-                t[r].style.height = e.data['datawrapper-height'][a] + 'px';
-            }
-        }
-      });
-    })();
-  </script>
-</div>
+<section class="graphic {size}">
+  {#if $$slots.title}
+    <div class="chatter-container">
+      <slot name="title" />
+    </div>
+  {/if}
+
+  <div class="datawrapper-chart">
+    <iframe
+      title="{title}"
+      aria-label="{ariaLabel}"
+      id="{id}"
+      src="{src}"
+      scrolling="{scrolling}"
+      frameborder="0"
+      style="width: 0; min-width: 100% !important; border: none;"></iframe>
+    <script type="text/javascript">
+      !(function () {
+        'use strict';
+        window.addEventListener('message', function (e) {
+          if (void 0 !== e.data['datawrapper-height']) {
+            var t = document.querySelectorAll('iframe');
+            for (var a in e.data['datawrapper-height'])
+              for (var r = 0; r < t.length; r++) {
+                if (t[r].contentWindow === e.source)
+                  t[r].style.height = e.data['datawrapper-height'][a] + 'px';
+              }
+          }
+        });
+      })();
+    </script>
+  </div>
+
+  {#if $$slots.notes}
+    <div class="chatter-container">
+      <slot name="notes" />
+    </div>
+  {/if}
+</section>
 
 <style lang="scss">
+  @import '~@reuters-graphics/style-theme-eisbaer/scss/components/containers/widths';
+  .chatter-container {
+    @extend .well;
+  }
   .datawrapper-chart {
-    // max-width: 660px;
     margin: auto;
-    //border: 1px solid red;
   }
 </style>
