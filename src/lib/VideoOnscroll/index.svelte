e84febf69a46a1f046c63386f0ba70e7368a4d6f
pkd2512 <pkd.dapacific@gmail.com>
2022-05-27 12:37:07 +0530

    basic scroll play

--- src/lib/VideoOnscroll/index.svelte
+++ src/lib/VideoOnscroll/index.svelte
@@ -1,4 +1,34 @@
 <script>
+  export let src = '';
+  export let size = 'wide';
+
+  let scrollY;
+  let time;
+  let duration;
+
+  $: {
+    const totalScroll =
+      document.documentElement.scrollHeight - window.innerHeight;
+    time = duration * (scrollY / totalScroll);
+  }
 </script>
 
-<section class="video-onscroll"></section>
+<svelte:window bind:scrollY />
+
+<section class="video-onscroll graphic {size}">
+  <div class="video-wrapper">
+    <video
+      bind:currentTime="{time}"
+      bind:duration
+      preload="metadata"
+      muted
+      src="{src}"
+      type="video/mp4"></video>
+  </div>
+</section>
+
+<style lang="scss">
+  video {
+    width: 100%;
+  }
+</style>
