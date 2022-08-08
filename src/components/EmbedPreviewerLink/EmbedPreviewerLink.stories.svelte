d46ff2ca6437365d8bca2a88298f8c32929c35e7
Jon McClure <jon.r.mcclure@gmail.com>
2022-08-08 21:33:32 +0100

    EndNotes

---
+++ src/components/EmbedPreviewerLink/EmbedPreviewerLink.stories.svelte
@@ -0,0 +1,31 @@
+<script>
+  import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+
+  // @ts-ignore
+  import componentDocs from './stories/docs/component.md?raw';
+
+  import EmbedPreviewerLink from './EmbedPreviewerLink.svelte';
+
+  import {
+    withComponentDocs
+  } from '$docs/utils/withParams';
+
+  const meta = {
+    title: 'Components/EmbedPreviewerLink',
+    component: EmbedPreviewerLink,
+    ...withComponentDocs(componentDocs),
+  };
+</script>
+
+<Meta {...meta} />
+
+<Template let:args>
+    <EmbedPreviewerLink {...args} />
+</Template>
+
+<Story
+  name="Default"
+  args={{
+    dev: true
+  }}
+/>
---
+++ src/components/EndNotes/EndNotes.stories.svelte
@@ -0,0 +1,37 @@
+<script>
+  import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+
+  // @ts-ignore
+  import componentDocs from './stories/docs/component.md?raw';
+
+  import EndNotes from './EndNotes.svelte';
+
+  import {
+    withComponentDocs
+  } from '$docs/utils/withParams';
+
+  const meta = {
+    title: 'Components/EndNotes',
+    component: EndNotes,
+    ...withComponentDocs(componentDocs),
+  };
+</script>
+
+<Meta {...meta} />
+
+<Template let:args>
+  <EndNotes {...args} />
+</Template>
+
+<Story
+  name="Default"
+  args={{
+    text: `### Source
+
+Reuters research.
+
+### Credits
+
+People.`
+  }}
+/>
