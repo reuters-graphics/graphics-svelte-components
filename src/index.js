fe3a5e54ee21714e0210ff33a2bd2db1a70f6b64
Jon McClure <jon.r.mcclure@gmail.com>
2022-08-08 22:45:37 +0100

    lineup index import exports

--- src/index.js
+++ src/index.js
@@ -1,27 +1,26 @@
+// export { default as Ai2svelte } from './Ai2svelte/index.svelte';
+// export { default as BeforeAfter } from './BeforeAfter/index.svelte';
 export { default as BodyText } from './components/BodyText/BodyText.svelte';
+// export { default as DatawrapperChart } from './DatawrapperChart/index.svelte';
 export { default as EmbedPreviewerLink } from './components/EmbedPreviewerLink/EmbedPreviewerLink.svelte';
 export { default as EndNotes } from './components/EndNotes/EndNotes.svelte';
 // export { default as Framer } from './Framer/index.svelte';
-// export { default as Image } from './Image/index.svelte';
-export { default as PymChild } from './components/PymChild/PymChild.svelte';
-// export { default as Referrals } from './Referrals/index.svelte';
-// export { default as SEO } from './SEO/index.svelte';
-// export { default as Sharer } from './Sharer/index.svelte';
 export { default as Headline } from './components/Headline/Headline.svelte';
 // export { default as Hero } from './Hero/index.svelte';
-// export { default as Ai2svelte } from './Ai2svelte/index.svelte';
-// export { default as Scroller } from './Scroller/index.svelte';
-// export { default as BeforeAfter } from './BeforeAfter/index.svelte';
-// export { default as DatawrapperChart } from './DatawrapperChart/index.svelte';
+// export { default as Image } from './Image/index.svelte';
+export { default as PymChild } from './components/PymChild/PymChild.svelte';
 export { default as ReutersLogo } from './components/ReutersLogo/ReutersLogo.svelte';
-// export {
-//   default as SiteHeader,
-//   themes as SiteHeaderThemes
-// } from './SiteHeader/index.svelte';
+// export { default as Scroller } from './Scroller/index.svelte';
+// export { default as SEO } from './SEO/index.svelte';
+// export { default as Sharer } from './Sharer/index.svelte';
 // export {
 //   default as SiteFooter,
 //   themes as SiteFooterThemes
 // } from './SiteFooter/index.svelte';
+// export {
+//   default as SiteHeader,
+//   themes as SiteHeaderThemes
+// } from './SiteHeader/index.svelte';
 export { default as Spinner } from './components/Spinner/Spinner.svelte';
 // export { default as Video } from './Video/index.svelte';
 export { default as Visible } from './components/Visible/Visible.svelte';
